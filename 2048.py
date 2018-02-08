c288ec998799f760c9fc5e6166bd83b7def29ff7
yy <641016261@qq.com>
2018-02-08 17:07:22 +0800

    finish 2048

--- 2048.py
+++ 2048.py
@@ -2,13 +2,13 @@
 # -*- coding:utf-8 -*-
 
 import curses
-from random import randrang, choice
+from random import randrange, choice
 from collections import defaultdict
 
 letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
 actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
 
-actions_dict = dict(zip(letter_codes, action * 2))
+actions_dict = dict(zip(letter_codes, actions * 2))
 
 
 def transpose(field):
@@ -22,12 +22,12 @@ def invert(field):
 def get_user_action(keyboard):
     char = "N"
     while char not in actions_dict:
-        char = keyboard.gerch()
+        char = keyboard.getch()
     return actions_dict[char]
 
 
 class GameField(object):
-    def __init__(self, height=4, width=4, win=2048)
+    def __init__(self, height=4, width=4, win=2048):
         self.height = height
         self.width = width
         self.win_value = win
@@ -38,7 +38,7 @@ class GameField(object):
     # 随机生成一个2或4
     def spawn(self):
         new_element = 4 if randrange(100) > 89 else 2
-        (i, j) = choice([(i, j) for i in range(self.width) for j in range(self.height if self[i][j] == 0)])
+        (i, j) = choice([(i, j) for i in range(self.width) for j in range(self.height) if self.field[i][j] == 0])
         self.field[i][j] = new_element
 
     # 重置棋盘
@@ -56,29 +56,29 @@ class GameField(object):
         def move_row_left(row):
             # 合并非零单元
             def tighten(row):
-                new_row = [i for i in row if != 0]  # 另起一list，存储非零数值
+                new_row = [i for i in row if i != 0]  # 另起一list，存储非零数值
                 # 补上row应有的长度
-                new_row += [0 for i in range(len(row) - lebn(new_row))]
+                new_row += [0 for i in range(len(row) - len(new_row))]
                 return new_row
-        # 对临近元素合并
-
-        def merge(row):
-            pair = False
-            new_row = []
-            for i in range(len(row)):
-                if pair:
-                    new_row.append(2*row[i])
-                    self.score += 2 * roe[i]
-                    pair = False
-                else:
-                    if i + 1 < len(row) and row[i] == row[i + 1]:
-                        pair = True
-                        new_row.append(0)
+        
+            # 对临近元素合并
+            def merge(row):
+                pair = False
+                new_row = []
+                for i in range(len(row)):
+                    if pair:
+                        new_row.append(2*row[i])
+                        self.score += 2 * row[i]
+                        pair = False
                     else:
-                        new_row.append(row[i])
-            assert len(new_row) == len(row)
-            return new_row
-        return tighten(merge(tighten(row)))  # 先挤在一起再相加再挤在一起
+                        if i + 1 < len(row) and row[i] == row[i + 1]:
+                            pair = True
+                            new_row.append(0)
+                        else:
+                            new_row.append(row[i])
+                assert len(new_row) == len(row)
+                return new_row
+            return tighten(merge(tighten(row)))  # 先挤在一起再相加再挤在一起
 
         moves = {}
         moves['Left'] = lambda field:                              \
@@ -86,13 +86,14 @@ class GameField(object):
         moves['Right'] = lambda field:                              \
             invert(moves['Left'](invert(field)))
         moves['Up'] = lambda field:                              \
-            transpose(move['Left'](transpose(field)))
+            transpose(moves['Left'](transpose(field)))
         moves['Down'] = lambda field:                              \
             transpose(move['Right'](transpose(field)))
 
         if direction in moves:
             if self.move_is_possible(direction):
                 self.field = moves[direction](self.field)
+                self.spawn()
                 return True
             else:
                 return False
@@ -146,9 +147,9 @@ class GameField(object):
         def row_is_left_movable(row):
             # 检查是否可以移动或合并
             def change(i):
-                if row[i] == 0 and row[i] != 0:
+                if row[i] == 0 and row[i + 1] != 0:
                     return True
-                if row[i] != 0 and row[i] == row[i + 1]:
+                if row[i] != 0 and row[i + 1] == row[i]:
                     return True
                 return False
             
@@ -157,6 +158,7 @@ class GameField(object):
         check = {}
         check['Left'] = lambda field:                               \
                 any(row_is_left_movable(row) for row in field)
+
         check['Right'] = lambda field:                              \
                  check['Left'](invert(field))
 
@@ -171,4 +173,48 @@ class GameField(object):
         else:
             return False
 
+def main(stdscr):
     
+    def init():
+        game_field.reset()
+        return 'Game'
+
+    def not_game(state):
+        game_field.draw(stdscr)
+
+        action = get_user_action(stdscr)
+        responses = defaultdict(lambda: state)
+        responses['Restart'], responses['Exit'] = 'Init', 'Exit'
+        return responses[action]
+
+    def game():
+        game_field.draw(stdscr)
+        
+        action = get_user_action(stdscr)
+
+        if action == 'Restart':
+            return 'Init'
+        if action == 'Exit':
+            return 'Exit'
+        if game_field.move(action): 
+            if game_field.is_win():
+                return 'Win'
+            if game_field.is_gameover():
+                return 'Gameover'
+        return 'Game'
+
+    state_actions = {
+            'Init': init,
+            'Win': lambda: not_game('Win'),
+            'Gameover': lambda: not_game('Gameover'),
+            'Game': game
+        }
+
+    curses.use_default_colors()
+    game_field = GameField(win=32)
+
+    state = 'Init'
+    while state != 'Exit':
+        state = state_actions[state]()
+
+curses.wrapper(main)
